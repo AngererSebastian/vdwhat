c2612c0e775bb8b79e6e531bc6c0c0f9c93a98f6
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2022-05-01 20:24:32 +0200

    fix some parsing errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(trait_alias)]
+use ariadne::{Label, Report, ReportKind, Source};
 use chumsky::prelude::*;
 
 const TEST_DATA: &'static str = include_str!("../test");
@@ -10,8 +11,16 @@ fn main() {
     //let result = ignore_n(9).then(end()).parse(test);
     match result {
         Ok(parsed) => println!("parsed {:?}", parsed),
-        Err(err) => err.iter().for_each(|e| println!("{:?}", e)),
-    }
+        Err(err) => err.iter().for_each(|e| {
+            /*Report::build::<()>(ReportKind::Error, (), e.span().start)
+            .with_message::<String>(format!("{:?}", e.reason()))
+            .with_label(Label::new(e.span()))
+            .finish()
+            .print(Source::from(TEST_DATA))
+            .unwrap();*/
+            println!("{:?}", e);
+        }),
+    };
 }
 
 #[derive(Debug)]
@@ -20,8 +29,8 @@ struct Vda {
     lieferant: String,
     werk: String,
     abladestelle: String,
-    lieferabruf_alt: u32,
-    lieferabruf_neu: u32,
+    lieferabruf_alt: u64,
+    lieferabruf_neu: u64,
     sachnummer: String,
     mengeeinheit: String,
     rueckstandmenge: String,
@@ -31,8 +40,8 @@ struct Vda {
 
 #[derive(Debug)]
 struct Abruf {
-    date: u32,
-    amount: u32,
+    date: u64,
+    amount: u64,
 }
 
 fn parser() -> impl VdaParser<Vda> {
@@ -74,13 +83,13 @@ fn parse_511() -> impl VdaParser<Result511> {
         .ignore_then(n_alphanums(9))
         .then(n_alphanums(9))
         .map(|(kunde, lieferant)| Result511 { kunde, lieferant })
-        .then_ignore(ignore_n(83))
+        .then_ignore(ignore_n(106)) // + linefeed alway here
 }
 
 struct Result512 {
     werk_kunde: String,
-    lieferabruf_neu: u32,
-    lieferabruf_alt: u32,
+    lieferabruf_neu: u64,
+    lieferabruf_alt: u64,
     sachnummer_kunde: String,
     abladestelle: String,
     mengeneinheit: String,
@@ -99,7 +108,7 @@ fn parse_512() -> impl VdaParser<Result512> {
         .then(n_alphanums(5)) //abladestelle
         .then_ignore(n_alphanums(4)) // zeichen kunde
         .then(n_alphanums(2)) // mengeneinheit
-        .then_ignore(n_alphanums(25))
+        .then_ignore(n_alphanums(26)) // + linefeed
         .map(
             |(
                 (
@@ -122,18 +131,18 @@ fn parse_513() -> impl VdaParser<Vec<Abruf>> {
     header(513)
         .then_ignore(ignore_n(43))
         .ignore_then(abruf().repeated().exactly(5))
-        .then_ignore(ignore_n(6))
+        .then_ignore(ignore_n(6)) // linefeed
 }
 
 fn parse_514() -> impl VdaParser<Vec<Abruf>> {
     header(514)
         .ignore_then(abruf().repeated().exactly(8))
-        .then_ignore(ignore_n(2))
+        .then_ignore(ignore_n(4)) // linefeed
 }
 
 fn parse_515() -> impl VdaParser<()> {
     header(515)
-        .then_ignore(ignore_n(123))
+        .then_ignore(ignore_n(124)) // linefeed
         .then(parse_517().or_not())
         .ignored()
 }
@@ -141,7 +150,7 @@ fn parse_515() -> impl VdaParser<()> {
 fn parse_517() -> impl VdaParser<()> {
     recursive(|parser| {
         header(517)
-            .then_ignore(ignore_n(123))
+            .then_ignore(ignore_n(124)) // linefeed
             .then(parser.or(parse_518()))
             .ignored()
     })
@@ -149,17 +158,17 @@ fn parse_517() -> impl VdaParser<()> {
 
 fn parse_518() -> impl VdaParser<()> {
     header(518)
-        .then_ignore(ignore_n(123))
+        .then_ignore(ignore_n(124)) // linefeed
         .then(parse_512())
         .then(parse_513())
         .ignored()
 }
 
 fn parse_519() -> impl VdaParser<()> {
-    header(519).then_ignore(ignore_n(123)).ignored()
+    header(519).then_ignore(ignore_n(124)).ignored()
 }
 
-fn header(code: u32) -> impl VdaParser<()> {
+fn header(code: u64) -> impl VdaParser<()> {
     just(code.to_string())
         .labelled("header")
         .then_ignore(counted_number(2))
@@ -173,13 +182,10 @@ fn abruf() -> impl VdaParser<Abruf> {
 }
 
 fn abruf_chain() -> impl VdaParser<Vec<Abruf>> {
-    parse_514().then_ignore(parse_515().or_not())
-    //.repeated()
-    //.map(|vs| vs.into_iter().flatten().collect())
-}
-
-fn ignore_51213() -> impl VdaParser<()> {
-    parse_512().then(parse_513()).ignored()
+    parse_514()
+        .then_ignore(parse_515().or_not())
+        .repeated()
+        .map(|vs| vs.into_iter().flatten().collect())
 }
 
 fn ignore_n(n: usize) -> impl VdaParser<()> {
@@ -189,18 +195,46 @@ fn ignore_n(n: usize) -> impl VdaParser<()> {
 fn n_alphanums(n: usize) -> impl VdaParser<String> {
     filter::<_, _, Simple<char>>(|c| char::is_ascii(c))
         .labelled("n alphanums")
-        //.repeated()
-        //.exactly(n)
-        //.collect::<String>()
+        .repeated()
+        .exactly(n)
+        .collect::<String>()
         .map(|c| String::from(c))
 }
 
-fn counted_number(n: usize) -> impl VdaParser<u32> {
-    filter::<_, _, Simple<char>>(char::is_ascii_digit)
+fn counted_number(n: usize) -> impl VdaParser<u64> {
+    filter::<_, _, Simple<char>>(|c| char::is_ascii_digit(c) || *c == ' ')
         .labelled("counted number")
-        //.repeated()
-        //.exactly(n)
-        //.collect::<String>()
-        //.try_map(|s, span| s.parse().map_err(|e| Simple::custom(span, e)))
-        .to(1)
+        .repeated()
+        .exactly(n)
+        .collect::<String>()
+        .try_map(|s, span| s.trim_end().parse().map_err(|e| Simple::custom(span, e)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn counted_number_1() {
+        let input = "12 34";
+        let rslt = counted_number(4).parse(input);
+
+        rslt.unwrap_err();
+    }
+
+    #[test]
+    fn counted_number_2() {
+        let input = "1234";
+        let rslt = counted_number(4).parse(input).unwrap();
+
+        assert_eq!(1234, rslt)
+    }
+
+    #[test]
+    fn counted_number_3() {
+        let input = "1234";
+        let rslt = counted_number(5).parse(input);
+
+        rslt.unwrap_err();
+    }
 }
